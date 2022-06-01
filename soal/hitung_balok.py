6af3eb0669dd56d26f9ccfac0769489c5c48ed24
nurulfauzh <nrulfauzh@gmail.com>
2022-06-01 23:38:38 +0700

    nurul

---
+++ soal/hitung_balok.py
@@ -0,0 +1,12 @@
+from balok import Balok
+
+class Hitung:
+
+    def hitung_luas(self,balok:Balok)->float:
+        return ((balok.get_panjang() * balok.get_lebar()) + (balok.get_panjang() * balok.get_tinggi()) + (balok.get_lebar()*balok.get_tinggi()))*2
+
+    def hitung_volume(self,balok:Balok)->float:
+        return (balok.get_panjang() * balok.get_lebar() * balok.get_tinggi())
+
+    def hitung_keliling(self,balok:Balok)->float:
+        return (balok.get_panjang() + balok.get_lebar()+balok.get_tinggi())*4
