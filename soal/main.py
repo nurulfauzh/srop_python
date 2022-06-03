2152053cc806c685eb4c4365f4af2536c1c5b149
nurulfauzh <nrulfauzh@gmail.com>
2022-06-03 09:57:38 +0700

    nurul

---
+++ soal/main.py
@@ -0,0 +1,13 @@
+
+from balok import Balok 
+from hitung_balok import Hitung
+from hasil_balok import HasilBalok
+
+
+balok = Balok()
+hitung_balok = Hitung()
+hasil_balok = HasilBalok()
+
+hasil_balok.show_panjang(balok, HasilBalok)
+hasil_balok.show_lebar(balok, HasilBalok) 
+hasil_balok.show_tinggi(balok, HasilBalok)
